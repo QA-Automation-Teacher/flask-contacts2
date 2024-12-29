687e32e718815d12dd87f5d141cea881feb41d00
gagishmagi <gagi.shmagi@gmail.com>
2024-12-29 10:47:06 +0200

    Added app context to migration

--- migrations.py
+++ migrations.py
@@ -1,20 +1,22 @@
 from models import db, Contacts
 from faker import Factory
+from app import app
 
-fake = Factory.create()
-# Spanish
-#fake = Factory.create('es_ES')
-# Reload tables
-db.drop_all()
-db.create_all()
-# Make 100 fake contacts
-for num in range(100):
-    fullname = fake.name().split()
-    name = fullname[0]
-    surname = ' '.join(fullname[1:])
-    email = fake.email()
-    phone = fake.phone_number()
-    # Save in database
-    mi_contacto = Contacts(name, surname, email, phone)
-    db.session.add(mi_contacto)
-    db.session.commit()
+with app.app_context():
+    fake = Factory.create()
+    # Spanish
+    #fake = Factory.create('es_ES')
+    # Reload tables
+    db.drop_all()
+    db.create_all()
+    # Make 100 fake contacts
+    for num in range(100):
+        fullname = fake.name().split()
+        name = fullname[0]
+        surname = ' '.join(fullname[1:])
+        email = fake.email()
+        phone = fake.phone_number()
+        # Save in database
+        mi_contacto = Contacts(name, surname, email, phone)
+        db.session.add(mi_contacto)
+        db.session.commit()
