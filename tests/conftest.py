9cf9ced67cacac8d5092d3e994cec086574db61c
gagishmagi <gagi.shmagi@gmail.com>
2024-12-29 13:54:54 +0200

    Added pytest configuration + integration test

---
+++ tests/conftest.py
@@ -0,0 +1,22 @@
+import pytest
+import sys
+import os
+import json
+
+
+
+src_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+if src_path not in sys.path:
+    sys.path.insert(0, src_path)
+
+
+from app import app
+
+
+@pytest.fixture
+def client():
+    with app.app_context(): 
+        app.config.update({
+            "TESTING": True
+        })
+        yield app.test_client()
---
+++ tests/integrations/test_contacts_integration.py
@@ -0,0 +1,14 @@
+BASE_URL = "http://localhost:5000"
+
+def setup_module(module):
+    pass
+
+
+def teardown_module(module):
+    pass
+
+
+def test_get_contacts(client):
+    response = client.get(BASE_URL + "/contacts")
+    assert response.status_code == 200
+    assert isinstance(response.get_json(), list)
